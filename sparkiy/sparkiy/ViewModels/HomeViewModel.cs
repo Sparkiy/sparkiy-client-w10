2fc0c2b7eb3cb21a509533b37dddb8a824ee4f84
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2015-08-12 20:22:23 +0200

    DI and TitleBar service

---
+++ sparkiy/sparkiy/ViewModels/HomeViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using sparkiy.Services.UI;
+
+namespace sparkiy.ViewModels
+{
+	/// <summary>
+	/// Page view mode contract.
+	/// </summary>
+	public interface IPageViewModel
+	{
+		/// <summary>
+		/// Occurs on page loaded.
+		/// </summary>
+		Task LoadedAsync();
+	}
+
+	/// <summary>
+	/// Home view model contract.
+	/// </summary>
+	public interface IHomeViewModel : IPageViewModel
+	{
+	}
+
+	/// <summary>
+	/// Home view model.
+	/// </summary>
+	public sealed class HomeViewModel : IHomeViewModel
+	{
+		private readonly ITitleBarService titleBarService;
+
+
+		public HomeViewModel(ITitleBarService titleBarService)
+		{
+			if (titleBarService == null) throw new ArgumentNullException(nameof(titleBarService));
+
+			this.titleBarService = titleBarService;
+		}
+
+
+		/// <summary>
+		/// Occurs on page loaded.
+		/// </summary>
+		public async Task LoadedAsync()
+		{
+			// Set title bar color
+			this.titleBarService.SetAccentColor();
+		}
+	}
+}
---
+++ sparkiy/sparkiy/Views/HomePage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using sparkiy.ViewModels;
+using sparkiy.ViewModels.Utilities;
+
+namespace sparkiy.Views
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class HomePage : Page
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="HomePage"/> class.
+		/// </summary>
+		public HomePage()
+		{
+			// Retrieve view model
+			this.ViewModel = ViewModelResolver.Home;
+
+			// Trigger view model loaded on page loaded
+			this.Loaded += (sender, args) => this.ViewModel.LoadedAsync();
+
+			this.InitializeComponent();
+		}
+
+		/// <summary>
+		/// Gets the view model.
+		/// </summary>
+		/// <value>
+		/// The view model.
+		/// </value>
+		public IHomeViewModel ViewModel { get; }
+	}
+}
