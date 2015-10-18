84771c144bca3e0924718f823585f1873d81d826
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2015-10-19 00:05:40 +0200

    Added leftover class

---
+++ sparkiy/sparkiy/ViewModels/Devices/IoT/Windows/DeviceSetupViewModel.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight.Command;
+
+namespace sparkiy.ViewModels.Devices.IoT.Windows
+{
+	/// <summary>
+	/// Windows device setup view model.
+	/// </summary>
+	public interface IDeviceSetupViewModel : IPageViewModel
+	{
+		/// <summary>
+		/// Occurs when progress is made.
+		/// </summary>
+		event DeviceSetupProgressEventHandler OnProgress;
+
+		/// <summary>
+		/// Gets the progress forward command.
+		/// </summary>
+		/// <value>
+		/// The progress forward command.
+		/// </value>
+		RelayCommand ProgressForwardCommand { get; }
+
+		/// <summary>
+		/// Gets the progress backward command.
+		/// </summary>
+		/// <value>
+		/// The progress backward command.
+		/// </value>
+		RelayCommand ProgressBackwardCommand { get; }
+	}
+
+	/// <summary>
+	/// Windows device setup view model.
+	/// </summary>
+	public sealed class DeviceSetupViewModel : IDeviceSetupViewModel
+	{
+		private DeviceSetupSteps? currentStep;
+
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="DeviceSetupViewModel"/> class.
+		/// </summary>
+		public DeviceSetupViewModel()
+		{
+			// Initialize commands
+			this.ProgressForwardCommand = new RelayCommand(this.ProgressForward);
+			this.ProgressBackwardCommand = new RelayCommand(this.ProgressBackward);
+		}
+
+
+		/// <summary>
+		/// Occurs on page loaded.
+		/// </summary>
+		public async Task LoadedAsync()
+		{
+			// Go to initial step
+			this.ProgressForward();
+		}
+
+		/// <summary>
+		/// Raises the on progress event.
+		/// </summary>
+		/// <param name="nextStep">The next step.</param>
+		/// <remarks>
+		/// Current step atribute is set to value of current step variable of this instance.
+		/// </remarks>
+		private void RaiseOnProgressEvent(DeviceSetupSteps nextStep)
+		{
+			this.OnProgress?.Invoke(this, new DeviceSetupProgressEventArgs(this.currentStep, nextStep));
+		}
+
+		/// <summary>
+		/// Progresses forward.
+		/// </summary>
+		public void ProgressForward()
+		{
+			DeviceSetupSteps nextStep;
+
+			// Determine whether we should start from beginning or 
+			// move forward with next step.
+			if (!this.currentStep.HasValue)
+			{
+				nextStep = DeviceSetupSteps.Start;
+			}
+			else
+			{
+				// Determine next step
+				// ReSharper disable once SwitchStatementMissingSomeCases
+				switch (this.currentStep.Value)
+				{
+					case DeviceSetupSteps.Start:
+						nextStep = DeviceSetupSteps.FindingDevice;
+						break;
+					case DeviceSetupSteps.FindingDevice:
+						nextStep = DeviceSetupSteps.ConfirmDevice;
+						break;
+					case DeviceSetupSteps.ConfirmDevice:
+						nextStep = DeviceSetupSteps.CustomizeDevice;
+						break;
+					case DeviceSetupSteps.CustomizeDevice:
+						nextStep = DeviceSetupSteps.Installation;
+						break;
+					case DeviceSetupSteps.Installation:
+						nextStep = DeviceSetupSteps.Success;
+						break;
+					default:
+						nextStep = DeviceSetupSteps.Start;
+						break;
+				}
+			}
+
+			// Raise changes event and set new step as current
+			this.RaiseOnProgressEvent(nextStep);
+			this.currentStep = nextStep;
+		}
+
+		/// <summary>
+		/// Progresses backward.
+		/// </summary>
+		public void ProgressBackward()
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Occurs when progress is made.
+		/// </summary>
+		public event DeviceSetupProgressEventHandler OnProgress;
+
+		/// <summary>
+		/// Gets the progress forward command.
+		/// </summary>
+		/// <value>
+		/// The progress forward command.
+		/// </value>
+		public RelayCommand ProgressForwardCommand { get; private set; }
+
+		/// <summary>
+		/// Gets the progress backward command.
+		/// </summary>
+		/// <value>
+		/// The progress backward command.
+		/// </value>
+		public RelayCommand ProgressBackwardCommand { get; private set; }
+	}
+}
